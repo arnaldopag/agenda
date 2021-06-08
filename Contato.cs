fafa7c6a7cffa918775932716a58bb7437475709
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-06-08 18:57:15 -0300

    quase lá

---
+++ Contato.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace agenda
+{
+    public class Contato
+    {
+        private List<ItemContato> ItemContato;
+    
+        public Contato(){
+            ItemContato = new List<ItemContato>();
+        }
+
+    }
+}
--- itemContato.cs
+++ itemContato.cs
@@ -1,10 +1,15 @@
 namespace agenda
 {
-    public class itemContato
+    public class ItemContato
     {
-        string nome;
-        string email;
-        string endereco;
-        
+        public string nome, email, endereco;
+
+        public ItemContato (string n, string e, string d){
+            nome = n;
+            email = e;
+            endereco = d;
+
+        }
+
     }
 }
